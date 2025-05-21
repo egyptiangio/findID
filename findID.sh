92eb68e8a200fc2220b72848c16e502eb58b0b6f
root <root@localhost.localdomain>
2025-05-20 20:49:48 -0400

    Update findID.sh to v1.1: improved table layout, logo fallback if viu dependency not installed

--- findID.sh
+++ findID.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# station-lookup.sh v1.0 (restored working version with local config support)
+# station-lookup.sh v1.1
 
 #####################
 # Configuration
@@ -10,12 +10,19 @@ scriptName="$(basename "${BASH_SOURCE[0]}" .sh)"
 CONFIG_FILE="$scriptDir/${scriptName}.conf"
 
 # Dependencies check
-for dep in jq curl viu; do
+for dep in jq curl; do
   if ! command -v "$dep" &> /dev/null; then
     echo "Error: '$dep' is not installed."
     exit 1
   fi
 done
+
+# Check for viu separately, allow fallback
+if ! command -v viu &> /dev/null; then
+  echo "Notice: 'viu' is not installed. Channel logos will not be shown."
+  echo "To enable logo previews, install 'viu': https://github.com/atanunq/viu"
+  wantImages=false
+fi
 
 # Load or create config
 if [[ -f "$CONFIG_FILE" ]]; then
@@ -84,13 +91,15 @@ while true; do
     *) stationQuery="$userInput" ;;
   esac
 
-  # Encode station name and call sign separately
   encodedStationName="%22${stationQuery// /%20}%22"
   responseName=$(curl -s "http://${CHANNELS_DVR_IP}:${CHANNELS_DVR_PORT}/tms/stations/${encodedStationName}")
   responseCallSign=$(curl -s "http://${CHANNELS_DVR_IP}:${CHANNELS_DVR_PORT}/tms/stations/%22${stationQuery^^}%22")
 
-  # Merge responses and remove duplicates
-  response=$(echo "$responseName" "$responseCallSign" | jq -s 'add | unique_by(.stationId)')
+  response=$(echo "$responseName" "$responseCallSign" | jq -s 'add | unique_by(.stationId)' 2>/dev/null)
+  if [[ -z "$response" ]]; then
+    echo "Error: Malformed JSON response. Check DVR connectivity."
+    continue
+  fi
 
   count=$(echo "$response" | jq length)
   if [[ "$count" -eq 0 ]]; then
@@ -107,41 +116,36 @@ while true; do
 
   while [[ $index -lt $total ]]; do
     echo -e "\033[1;33m"
-    printf "%-10s %-12s %-40s %-15s %-7s %-10s %-12s\n" "TYPE" "CALLSIGN" "NAME" "AFFILIATE" "LANG" "VIDEO" "ID"
-    printf "%s\n" "--------------------------------------------------------------------------------------------------------------------------"
+    printf "%-40s %-12s %-10s %-15s\n" \
+      "NAME" "CALLSIGN" "VIDEO" "AFFILIATE"
+    printf "%s\n" "-------------------------------------------------------------------------------------------"
     echo -e "\033[0m"
 
     for ((i = 0; i < 5 && index < total; i++)); do
       obj="${matches[$index]}"
 
-      type=$(echo "$obj" | jq -r '.type // "N/A"')
       name=$(echo "$obj" | jq -r '.name // "N/A"')
       callSign=$(echo "$obj" | jq -r '.callSign // "N/A"')
-      stationId=$(echo "$obj" | jq -r '.stationId // "N/A"')
-      affiliate=$(echo "$obj" | jq -r '.affiliateCallSign // "N/A"')
-      logoUrl=$(echo "$obj" | jq -r '.preferredImage.uri // "N/A"')
       videoType=$(echo "$obj" | jq -r '.videoQuality.videoType // "N/A"')
-      language=$(echo "$obj" | jq -r '.broadcastLanguage // "N/A"')
+      affiliate=$(echo "$obj" | jq -r '.affiliateCallSign // "N/A"')
+      stationId=$(echo "$obj" | jq -r '.stationId // "N/A"')
 
-      if (( index % 2 == 0 )); then rowColor=""; else rowColor="\033[90m"; fi
-
-      echo -en "$rowColor"
-      printf "%-10s %-12s %-40s %-15s %-7s %-10s \033[35m%-12s\033[0m\n" \
-        "$type" "$callSign" "$name" "$affiliate" "$language" "$videoType" "$stationId"
-      echo -e "\033[0m"
+      printf "%-40s %-12s %-10s %-15s\n" \
+        "$name" "$callSign" "$videoType" "$affiliate"
 
       if $wantImages; then
-        if [[ "$logoUrl" != "N/A" ]]; then
+        logoUrl=$(echo "$obj" | jq -r '.preferredImage.uri // ""')
+        if [[ -n "$logoUrl" && "$logoUrl" != "" && "$logoUrl" != "null" ]]; then
           cleanUrl=$(echo "$logoUrl" | cut -d'?' -f1)
           fileName="$tmpDir/logo_${stationId}.png"
           curl -s -o "$fileName" "$cleanUrl"
           if [[ -s "$fileName" ]]; then
-            viu -w 40 "$fileName"
+            viu -h 5 "$fileName"
           else
-            echo "[No image found]"
+            echo "[No Logo Available]"
           fi
         else
-          echo "[No logo available]"
+          echo "[No Logo Available]"
         fi
       fi
 
